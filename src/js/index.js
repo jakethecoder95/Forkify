58ab4bbd3fd0bdca9f99a1555906973b38d00229
jakethecoder95 <95jacob07@gmail.com>
2018-10-29 11:26:51 -0700

    10/29/18 Search items done -> starting Recipe obj

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
-import * as searchView from '.views/Search';
-import { elements } from './views/base';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
  *  - Search object
@@ -12,20 +12,24 @@ const state = {};
 
 const controlSearch = async () => {
     // 1) Get query from view
-    const query = searchView.getInput
-    console.log(query);
+    const query = searchView.getInput();
 
     if (query) {
         // 2) New search obj and add to state
         state.search = new Search(query);
 
         // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
 
         // 4) Search for recipes
         await state.search.getResults();
 
         // 5) Render results on UI
-        console.log(state.search.result);
+        clearLoader();
+        searchView.renderResults(state.search.result);
     }
     
 }
@@ -33,4 +37,13 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline')
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
 });
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -10,9 +10,7 @@ export default class Search {
         const key = '6b7344ad770c974efb9a900a95993e1f';
         try {
             const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-            this.result = res.data.recipe;
-            //console.log(this.result); ////// TODO
-            console.log(res);
+            this.result = res.data.recipes;
         } catch (error) {
             alert(error);
         }
